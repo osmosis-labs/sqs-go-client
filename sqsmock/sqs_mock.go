5d315dabc4a426d6c4e670e9f47e224125e051f3
Roman <roman@osmosis.team>
2024-12-15 17:56:24 -0700

    rename

--- sqsmock/sqs_mock.go
+++ sqsmock/sqs_mock.go
@@ -9,7 +9,7 @@ import (
 // SQSMock is a mock implementation of the sqsclient.SQSClient interface.
 type SQSMock struct {
 	GetPricesFunc         func(ctx context.Context, options ...sqsclient.TokenPricesOption) (map[string]map[string]string, error)
-	GetRouteFunc          func(ctx context.Context, options ...sqsclient.RouterQuoteOption) (sqsclient.SQSQuoteResponse, error)
+	GetQuoteFunc          func(ctx context.Context, options ...sqsclient.RouterQuoteOption) (sqsclient.SQSQuoteResponse, error)
 	GetTokensMetadataFunc func(ctx context.Context) (map[string]sqsclient.OsmosisTokenMetadata, error)
 }
 
@@ -24,8 +24,8 @@ func (s *SQSMock) GetPrices(ctx context.Context, options ...sqsclient.TokenPrice
 
 // GetQuote implements sqsclient.SQSClient.
 func (s *SQSMock) GetQuote(ctx context.Context, options ...sqsclient.RouterQuoteOption) (sqsclient.SQSQuoteResponse, error) {
-	if s.GetRouteFunc != nil {
-		return s.GetRouteFunc(ctx, options...)
+	if s.GetQuoteFunc != nil {
+		return s.GetQuoteFunc(ctx, options...)
 	}
 
 	return sqsclient.SQSQuoteResponse{}, nil
